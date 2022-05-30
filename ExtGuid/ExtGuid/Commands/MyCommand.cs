b72427bead97a03e613c07cf2641e97a4488506b
dariecristian <71259662+dariecristian@users.noreply.github.com>
2022-05-30 11:51:42 +0300

    Uploaded

---
+++ ExtGuid/ExtGuid/Commands/MyCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace ExtGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if(selection.HasValue)
+            {
+                var guid = Guid.NewGuid().ToString();
+                docView.TextBuffer.Replace(selection.Value, guid);
+            }
+        }
+    }
+}
